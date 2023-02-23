9050679a4c36356c950109af51f2b4cb57ce5c5a
rubhain <bugrahantufekcioglu@gmail.com>
2023-02-23 20:03:27 +0300

    GET - DELETE - List

--- React/homework-23.02.23/src/components/SuppliersList.jsx
+++ React/homework-23.02.23/src/components/SuppliersList.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
 
 function SuppliersList() {
-  return (
-    <div>SuppliersList</div>
-  )
-}
+    const [suppliers, setSuppliers] = useState([]);
+
+    useEffect(() => {
+        getData();
+    }, [])
+
+    const getData = () => {
+        fetch(
+            "https://northwind.vercel.app/api/suppliers",
+          )
+            .then((res) => res.json())
+            .then((data) => {
+              setSuppliers(data);
+            })
+            .catch((err) => {
+              console.log("error ", err);
+            }); 
+    }
+ 
+    const deleteSupplier = (id) => {
+        fetch(
+            "https://northwind.vercel.app/api/suppliers/" + id,
+            {
+              method: "DELETE",
+            }
+          )
+          getData();
+    };
+
+    return (
+        <><table className='w3-table w3-striped w3-border'>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Company</th>
+                    <th>Contact</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    suppliers && suppliers.map(item => {
+                        return (
+                        <tr key={item.id} >
+                            <td>{item.id}</td>
+                            <td>{item.companyName}</td>
+                            <td>{item.contactName}</td>
+                            <td><button onClick={() => deleteSupplier(item.id)}>Delete</button></td>
+                        </tr>)
+                    })
+                }
+            </tbody>
+            </table>
+        </>
+    )
+    }
 
 export default SuppliersList
