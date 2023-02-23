127ee1b4e54daa06b2c01a3fcb336c746bba6b95
rubhain <bugrahantufekcioglu@gmail.com>
2023-02-23 23:24:17 +0300

    fetch - delete - map

--- React/homework-23.02.23/src/components/SuppliersList.jsx
+++ React/homework-23.02.23/src/components/SuppliersList.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
+import SupplierForm from './SupplierForm';
 
 function SuppliersList() {
     const [suppliers, setSuppliers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         getData();
@@ -14,9 +16,10 @@ function SuppliersList() {
             .then((res) => res.json())
             .then((data) => {
               setSuppliers(data);
+              setIsLoading(false)
             })
             .catch((err) => {
-              console.log("error ", err);
+              console.log("GET error ", err);
             }); 
     }
  
@@ -26,17 +29,27 @@ function SuppliersList() {
             {
               method: "DELETE",
             }
-          )
-          getData();
+          ).then(res => {
+            setIsLoading(true);
+            getData()
+        }).catch((err) => {
+            console.log("DELETE error ", err);
+          }); 
     };
 
     return (
-        <><table className='w3-table w3-striped w3-border'>
+        <><SupplierForm isLoading={isLoading} setIsLoading={setIsLoading} getData={getData}/>
+        <br />
+        {
+        isLoading ? <h1>Loading...</h1> :
+        <table className='w3-table w3-striped w3-border'>
             <thead>
                 <tr>
                     <th>Id</th>
                     <th>Company</th>
                     <th>Contact</th>
+                    <th>Title</th>
+                    <th>City</th>
                     <th>Delete</th>
                 </tr>
             </thead>
@@ -48,12 +61,15 @@ function SuppliersList() {
                             <td>{item.id}</td>
                             <td>{item.companyName}</td>
                             <td>{item.contactName}</td>
+                            <td>{item.contactTitle}</td>
+                            <td>{item.address.city}</td>
                             <td><button onClick={() => deleteSupplier(item.id)}>Delete</button></td>
                         </tr>)
                     })
                 }
             </tbody>
-            </table>
+        </table>
+        }
         </>
     )
     }
