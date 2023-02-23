e5a0c0731ff35c0840a583f98cf85e07d0570650
rubhain <bugrahantufekcioglu@gmail.com>
2023-02-23 23:24:25 +0300

    App

--- React/homework-23.02.23/src/App.js
+++ React/homework-23.02.23/src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import SupplierForm from "./components/SupplierForm";
 import SuppliersList from "./components/SuppliersList";
 
 function App() {
   return (
     <div className="App">
-      <SupplierForm />
       <SuppliersList />
     </div>
   );
