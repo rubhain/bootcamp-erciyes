1f1340e6fbd59d8f25e8fb46983fc57386e846d1
rubhain <bugrahantufekcioglu@gmail.com>
2023-02-23 19:42:50 +0300

    Components added

---
+++ React/homework-23.02.23/src/components/SupplierForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SupplierForm() {
+  return (
+    <div>SupplierForm</div>
+  )
+}
+
+export default SupplierForm
---
+++ React/homework-23.02.23/src/components/SuppliersList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SuppliersList() {
+  return (
+    <div>SuppliersList</div>
+  )
+}
+
+export default SuppliersList
